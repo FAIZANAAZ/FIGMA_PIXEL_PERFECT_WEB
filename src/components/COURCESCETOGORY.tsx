efebeaa61b3c644bd277fb0e43b0e3c4d22b86fd
FAIZANAAZ779 <faixanaaz@gmail.com>
2024-11-29 03:10:48 +0500

    update

--- src/components/COURCESCETOGORY.tsx
+++ src/components/COURCESCETOGORY.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import {coursescategory} from "../../constant/catogory"
-import {Course} from "../../constant/catogory"
+
 
 const COURCESCETOGORY = () => {
   
